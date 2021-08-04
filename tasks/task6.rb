99473b056a132a3dabd21cd2ec430a71ec30c78f
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-08-04 12:39:54 +0500

    Task 6: Corresponding season finding problem

--- tasks/task6.rb
+++ tasks/task6.rb
@@ -4,33 +4,42 @@ require "date"
 #           Task 6            |
 #------------------------------
 
-
-  
 def hemisphere_season(region, date)
 
   #Structure for storing the Date ranges. 
   seasons = {
     "S" => {
-        "spring" => [Date.parse("March 1"), Date.parse("May, 31")]
-        }
+        "autumn" => Date.parse("March 1")..Date.parse("May, 31"), 
+        "winter" => Date.parse("June, 1")..Date.parse("August, 31"), 
+        "spring" => Date.parse("September, 1").. Date.parse("November, 30"),
+        "summer" => Date.parse("December, 1") .. Date.parse("February, 28")
+        },
+
+    "N" => {
+        "spring" => Date.parse("March 1")..Date.parse("May, 31"), 
+        "summer" => Date.parse("June, 1")..Date.parse("August, 31"),
+        "autumn" => Date.parse("September, 1").. Date.parse("November, 30"),
+        "winter" => Date.parse("December, 1") .. Date.parse("February, 28"),
+        }    
     }
 
     region_struct = seasons[region]
-    date = Date.parse(date).to_s
+    date = Date.parse(date)
 
-    puts region_struct 
-    puts date
-
-    return region_struct
-
+    keys = region_struct.keys 
 
     #checking through all seasons
-    for key in region_struct.keys() do
+    for key in keys do
+
+        #date range for each season
         date_range = region_struct[key]
-        if(date >= date_range[0] && date <= date_range[1] ) do
-            return key.capitalize
+
+        #checking if given date is in the range
+        if(date_range.include?(date))
+            return key.to_s.capitalize
         end
+
     end
 
 end
-puts hemisphere_season("S", "April 2")
+puts hemisphere_season("N", "June, 30")
