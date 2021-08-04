bd57e29b56c6cc6df72a9a4c082009486996872b
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-08-04 15:09:13 +0500

    Task 11: Earn bonuses

---
+++ tasks/task11.rb
@@ -0,0 +1,29 @@
+#------------------------------
+#           Task 11           |
+#------------------------------
+
+
+
+
+
+
+
+def possible_bonus(first, second)
+    i = 1
+
+    while i <= 6
+        first += 1
+        if first === second
+            return true
+        end
+
+        i += 1
+    end
+
+    return false
+end
+
+
+puts possible_bonus(3, 7) 
+puts possible_bonus(1, 9)
+puts possible_bonus(5, 3) 
