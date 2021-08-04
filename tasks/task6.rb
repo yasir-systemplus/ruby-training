b5e76a8c06fa7bf822ad16009823fc6c942f273d
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-08-04 12:00:04 +0500

    ROR Training and First 5 tasks

--- tasks/task6.rb
+++ tasks/task6.rb
@@ -1,15 +1,36 @@
 require "date"
 
-
-
-
-
-
-
-
-
 #------------------------------
 #           Task 6            |
 #------------------------------
 
-puts Date.parse("March, 1")
+
+  
+def hemisphere_season(region, date)
+
+  #Structure for storing the Date ranges. 
+  seasons = {
+    "S" => {
+        "spring" => [Date.parse("March 1"), Date.parse("May, 31")]
+        }
+    }
+
+    region_struct = seasons[region]
+    date = Date.parse(date).to_s
+
+    puts region_struct 
+    puts date
+
+    return region_struct
+
+
+    #checking through all seasons
+    for key in region_struct.keys() do
+        date_range = region_struct[key]
+        if(date >= date_range[0] && date <= date_range[1] ) do
+            return key.capitalize
+        end
+    end
+
+end
+puts hemisphere_season("S", "April 2")
