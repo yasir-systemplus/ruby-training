8e9b7bf3c86b8948e0b9e8ad2607f84be19fd556
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-08-05 14:07:23 +0500

    Task 25: Sum of numbers in nested array.

---
+++ tasks/task25.rb
@@ -0,0 +1,10 @@
+#------------------------------
+#           Task 25           |
+#------------------------------
+
+def product(numbers)
+    return numbers.flatten!(1).inject(1) { |accumulate, current| accumulate * current }
+end
+
+puts product([[1,2],[1,1],[2,3]]) #12
+puts product([[10,2],[5,3],[2,3]]) #1800
